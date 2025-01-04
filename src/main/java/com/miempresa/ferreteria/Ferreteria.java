5e244f977cba2069fa48ecfdcbe6a840eaaf75fc
Ivan Terraza <162390663+Andysitoop@users.noreply.github.com>
2025-01-04 14:47:36 -0600

    We started project in NeatBeans with Java and MSQL Lite

---
+++ src/main/java/com/miempresa/ferreteria/Ferreteria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.miempresa.ferreteria;
+
+/**
+ *
+ * @author terra
+ */
+public class Ferreteria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
