cc9b5a72b70953fc1025f7c1ef9164d241814c34
unknown <santiagoesdras501@gmail.com>
2025-01-14 22:43:23 -0600

    refactor & repair fxmlLoader in app.java

--- src/main/java/com/mycompany/java23project/App.java
+++ src/main/java/com/mycompany/java23project/App.java
@@ -14,20 +14,22 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("login"), 640, 480);
+        scene = new Scene(loadFXML("fxml", "login"), 640, 480);
         stage.setScene(scene);
         stage.setTitle("Login Application");
         stage.show();
     }
 
     static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
+        scene.setRoot(loadFXML("fxml", "login"));
     }
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/com/mycompany/Views/" + fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
+public static Parent loadFXML(String folder, String fxml) throws IOException {
+    // Construir la ruta con la carpeta y el nombre del archivo FXML
+    String path = String.format("/com/mycompany/%s/%s.fxml", folder, fxml);
+    return FXMLLoader.load(App.class.getResource(path));
+}
+
 
     public static void main(String[] args) {
         launch();
